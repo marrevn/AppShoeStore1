a49941cd08a7237233d0a0ca7f7613407896c8dc
marrevn <sveta.i.volhina@gmail.com>
2025-12-19 12:48:56 +0500

    Добавление функционала FormAvtoriz

--- AppShoeStore/FormAvtoriz.Designer.cs
+++ AppShoeStore/FormAvtoriz.Designer.cs
@@ -77,6 +77,7 @@
             btnGuest.TabIndex = 5;
             btnGuest.Text = "Войти как гость";
             btnGuest.UseVisualStyleBackColor = false;
+            btnGuest.Click += BtnGuest_Click;
             // 
             // btnLogin
             // 
@@ -89,6 +90,7 @@
             btnLogin.TabIndex = 4;
             btnLogin.Text = "Войти";
             btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += BtnLogin_Click;
             // 
             // txtPassword
             // 
@@ -96,6 +98,7 @@
             txtPassword.Name = "txtPassword";
             txtPassword.Size = new Size(250, 26);
             txtPassword.TabIndex = 3;
+            txtPassword.UseSystemPasswordChar = true;
             // 
             // lbPassword
             // 
--- AppShoeStore/FormAvtoriz.cs
+++ AppShoeStore/FormAvtoriz.cs
@@ -1,11 +1,50 @@
+using AppShoeStore.Models;
+
 namespace AppShoeStore
 {
     public partial class FormAvtoriz : Form
     {
+        public User CurrentUser { get; private set; }
+        public bool IsGuest { get; private set; }
         public FormAvtoriz()
         {
             InitializeComponent();
         }
 
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtLogin.Text) || String.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("Введите логин и пароль", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            using (var db = new Models.AppContext())
+            {
+                var user = db.Users
+                    .Where(w => w.Login == txtLogin.Text && w.PasswordUser == txtPassword.Text)
+                    .FirstOrDefault();
+                if (user != null)
+                {
+                    CurrentUser = user;
+                    IsGuest = false;
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BtnGuest_Click(object sender, EventArgs e)
+        {
+            CurrentUser = null;
+            IsGuest = true;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
     }
 }
